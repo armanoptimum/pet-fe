3607c20dc25fbca4b7fe647de83697345dc90831
armanmikoyan <arman.mikoyan1@gmail.com>
2024-12-14 16:31:48 +0400

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,6 @@ import logo from '@/assets/icons/logo.svg';
 import searchIcon from '@/assets/icons/search.svg';
 import regionImg from '@/assets/images/region.svg';
 import arrowBottom from '@/assets/icons/arrow-bottom-small.svg';
-import heartIcon from '@/assets/icons/heart.svg';
-import cartIcon from '@/assets/icons/cart.svg';
-import loopIcon from '@/assets/icons/loop.svg';
-import loopMobileIcon from '@/assets/icons/loop-mobile.svg';
-import logoMobile from '@/assets/icons/logo-mobile.svg';
-import burger from '@/assets/icons/burger.svg';
 import {
   HeaderStyled,
   LeftListBarStyled,
@@ -23,15 +17,15 @@ import {
 } from './styles';
 import Button from '../shared/Button/Button';
 import { BUTTON_STYLES } from '../shared/Button/constants';
-import { listItemData } from './data';
+import { iconList, listItemData, mobileList } from './data';
 
 const Header = () => {
   return (
     <HeaderStyled>
       <MobileBar>
-        <Image alt="burger" src={burger} />
-        <Image alt="logo" src={logoMobile} />
-        <Image alt="search" src={loopIcon} />
+        {mobileList.map(({ src, alt }) => (
+          <Image alt={alt} src={src} />
+        ))}
       </MobileBar>
 
       <LeftListBarStyled>
@@ -59,9 +53,9 @@ const Header = () => {
       </RightListBarStyled>
 
       <IconBars>
-        <Image alt="favorite" src={heartIcon} />
-        <Image alt="cart" src={cartIcon} />
-        <Image alt="search" src={loopIcon} />
+        {iconList.map(({ src, alt }) => (
+          <Image alt={alt} src={src} />
+        ))}
       </IconBars>
     </HeaderStyled>
   );
