0888525ef8c2fc0afc350f2b03d4c0dc1ef97d3b
Arman Mikoyan <arman.mikoyan@optimumpartners.co>
2024-12-17 20:05:12 +0400

    ssr for styled components

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,26 +1,16 @@
 import Navbar from '@/components/Navbar';
 import './globals.css';
-import { Inter } from 'next/font/google';
 import Footer from '@/components/Footer';
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-});
-
-export const metadata = {
-  title: 'Monito',
-  description: 'Pet service',
-};
-
+import StyledComponentsRegistry from './lib/registry'
+ 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html>
       <body>
         <Navbar />
-        {children}
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         <Footer />
       </body>
     </html>
-  );
+  )
 }
