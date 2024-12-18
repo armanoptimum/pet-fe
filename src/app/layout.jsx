f1fc1c4d554598d7f6569e52d5bd8c57057faf05
Arman Mikoyan <arman.mikoyan@optimumpartners.co>
2024-12-18 20:17:49 +0400

    styled

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,13 @@
 import Navbar from '@/components/Navbar';
 import './globals.css';
 import Footer from '@/components/Footer';
-import StyledComponentsRegistry from './lib/registry';
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
         <Navbar />
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        {children}
         <Footer />
       </body>
     </html>
